c7f13716dcb3e595729505638b35fda85bd375c2
Jodongjin <ehdwlsdlwkd@naver.com>
2023-10-27 22:05:56 +0900

    Feat: 회원가입 완료

--- sbb/src/main/java/com/example/sbb/user/UserController.java
+++ sbb/src/main/java/com/example/sbb/user/UserController.java
@@ -2,6 +2,7 @@ package com.example.sbb.user;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,14 +25,25 @@ public class UserController {
         if (bindingResult.hasErrors()) {
             return "signup_form";
         }
-
         if (!userCreateForm.getPassword().equals(userCreateForm.getPasswordCheck())) {
+            // bindingResult.rejectValue(필드명, 오류코드, 에러메시지): 특정 필드의 오류를 잡을 때
             bindingResult.rejectValue("passwordCheck", "passwordInCorrect",
                     "2개의 패스워드가 일치하지 않습니다.");
             return "signup_form";
         }
-
         userService.create(userCreateForm.getUsername(), userCreateForm.getEmail(), userCreateForm.getPassword());
+
+        try {
+            userService.create(userCreateForm.getUsername(), userCreateForm.getEmail(), userCreateForm.getPassword());
+        } catch (DataIntegrityViolationException e) { // 이메일 중복
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", "이미 등록된 사용자입니다.");
+            return "signup_form";
+        } catch (Exception e) {
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", e.getMessage());
+            return "signup_form";
+        }
 
         return "redirect:/";
     }
